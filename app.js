cdc8194e6876780dc79f9325498598a15111e864
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2017-09-30 02:05:25 +0500

    Forms Connected With Firebase

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 var emailInput = document.getElementById("signUpEmail");
 var passwordInput = document.getElementById("signUpPass");
-var nameInput = document.getElementById("userName");
+var signInEmailInput = document.getElementById("signInEmail");
+var signInPasswordInput = document.getElementById("signInPass");
+var nameInput = document.getElementById("UserNameInput");
 var database = firebase.database();
 var auth = firebase.auth();
 
-function signup() {
+function signUp() {
     var email = emailInput.value;
     var password = passwordInput.value;
     var name = nameInput.value;
 
     auth.createUserWithEmailAndPassword(email, password)
-        .then(function (user) {
-            var currentUser = {
-                name: name,
-                email: email,
-            }
-            localStorage.setItem('currentUser', JSON.stringify(currentUser));
-            location = 'login.html';
-        })
-        .catch(function (error) {
-            console.log(error.message);
-        })
-
-
+    .then(function (user) {
+        var currentUser = {
+            name: name,
+            email: email,
+        }
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        location = 'login.html';
+    })
+    .catch(function (error) {
+        console.log(error.message);
+    })
 }
 
 function login() {
-    var email = emailInput.value;
-    var password = passwordInput.value;
+    var email = signInEmailInput.value;
+    var password = signInPasswordInput.value;
 
     auth.signInWithEmailAndPassword(email, password)
         .then(function (user) {
